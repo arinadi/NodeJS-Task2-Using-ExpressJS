080c2bdd85c4ab85866726f087ed2fc26fd6aaaa
arinadi <arinadi.nur@gmail.com>
2019-06-16 00:11:45 +0700

    finish

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+
+const app = express();
+
+// app.use(bodyParser.urlencoded());
+
+app.use((req, res, next) => {
+    console.log("always run");
+    next(); //next process
+});
+app.use('/users', (req, res, next) => {
+    console.log("masuk "+req.baseUrl);
+    res.send(`Page "User"`); //simple response
+});
+app.use('/',(req, res, next) => {
+    console.log("masuk "+req.baseUrl);
+    res.send(`Halo dari expressJS`); //simple response
+});
+
+const port = 3000;
+app.listen(port);
